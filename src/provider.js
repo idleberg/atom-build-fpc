860900a18a58e1c9cb07b6e6de05e982cf6f5ae4
idleberg <git@idleberg.com>
2021-03-20 01:30:45 +0100

    use name from manifest

--- src/provider.js
+++ src/provider.js
@@ -1,3 +1,4 @@
+import meta from '../package.json';
 import { configSchema, getConfig } from './config';
 import { EventEmitter } from 'events';
 import { satisfyDependencies } from 'atom-satisfy-dependencies';
@@ -66,6 +67,6 @@ export function provideBuilder() {
 // This package depends on build, make sure it's installed
 export function activate() {
   if (getConfig('manageDependencies') === true) {
-    satisfyDependencies('build-fpc');
+    satisfyDependencies(meta.name);
   }
 }
