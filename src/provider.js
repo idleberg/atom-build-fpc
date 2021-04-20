1b7a3410b80f0c0c22cede8f55ddb7ca8db7be95
idleberg <git@idleberg.com>
2021-04-20 22:43:08 +0200

    use which module, logger

--- src/provider.js
+++ src/provider.js
@@ -1,9 +1,9 @@
-import meta from '../package.json';
 import { configSchema, getConfig } from './config';
 import { EventEmitter } from 'events';
 import { satisfyDependencies } from 'atom-satisfy-dependencies';
-import { spawnSync } from 'child_process';
-import { which } from './util';
+import Logger from './log';
+import meta from '../package.json';
+import which from 'which';
 
 export { configSchema as config };
 
@@ -21,15 +21,17 @@ export function provideBuilder() {
 
     isEligible() {
       if (getConfig('alwaysEligible') === true) {
+        Logger.log('Always eligible');
         return true;
       }
 
-      const cmd = spawnSync(which(), ['fpc']);
-      if (!cmd?.stdout?.toString()) {
-        return false;
+      if (which.sync('fpc', { nothrow: true })) {
+        Logger.log('Build provider is eligible');
+        return true;
       }
 
-      return true;
+      Logger.error('Build provider isn\'t eligible');
+      return false;
     }
 
     settings() {
@@ -64,9 +66,15 @@ export function provideBuilder() {
   };
 }
 
-// This package depends on build, make sure it's installed
 export function activate() {
+  Logger.log('Activating package');
+
+  // This package depends on build, make sure it's installed
   if (getConfig('manageDependencies') === true) {
     satisfyDependencies(meta.name);
   }
+}
+
+export function deactivate() {
+  Logger.log('Deactivating package');
 }
